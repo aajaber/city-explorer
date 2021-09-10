f67b5e9880683e626c1bda91b1ee1aa9b3f7c109
aajaber <aajaber.97@gmail.com>
2021-09-11 01:19:24 +0300

    lab06 updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,85 @@
-
 import './App.css';
-import InputFeild from './components/Inputpage';
+import axios from 'axios';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
-function App() {
-  return (
-    <div >
-      {/* <h1>
-        this is from app .js
 
-      </h1> */}
 
-      <InputFeild />
-    </div>
-  );
+class App extends React.Component {
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+      cityName: '',
+      cityData: '',
+      displayed: false,
+      errormessege: false
+
+    }
+  }
+
+  
+  formSubmit = async (e) => {
+    e.preventDefault();
+    const dataUrl = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_IQ_KEY}&q=${this.state.cityName}&format=json`;
+    try {
+
+      let locresult = await axios.get(dataUrl);
+
+
+      this.setState({
+        cityData: locresult.data[0],
+        displayed: true
+
+      })
+    }
+    catch {
+      this.setState({
+        displayed: false,
+        errormessege: true
+      })
+    }
+  }
+
+  
+  cityNameHandeler = (event) => {
+    this.setState({
+      cityName: event.target.value
+    })
+  }
+
+  render() {
+    const mapUrl = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_IQ_KEY}&q=${this.state.cityData.lat, this.state.cityData.long}&zoom=1-18`;
+
+    return (
+      <div>
+        <h1 style={{ textAlign: 'center' }}>City Explorer</h1>
+
+        <Form onSubmit={this.formSubmit}>
+          <Form.Control type="text" placeholder="Enter City Name" onChange={this.cityNameHandeler} />
+          <Button variant="primary" type="submit">Submit</Button>
+        </Form>
+
+        <p style={{ textAlign: 'center' }}>{this.state.cityData.display_name}</p>
+        <p style={{ textAlign: 'center' }}>{this.state.cityData.lat}</p>
+        <p style={{ textAlign: 'center' }}>{this.state.cityData.lon}</p>
+        {
+          this.state.displayed && <img src={mapUrl}
+            alt='map'
+          />
+
+        }
+        {
+          this.state.errormessege &&
+          <p >Faild to load Data !</p>
+        }
+      </div>
+    );
+  }
+
 }
-
 export default App;
--- src/components/Inputpage.js
+++ src/components/Inputpage.js
@@ -1,99 +1,99 @@
-import axios from 'axios';
-import React from 'react';
+// import axios from 'axios';
+// import React from 'react';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Form'
-import Card from 'react-bootstrap/Card'
-
-
-
-class InputFeild extends React.Component {
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// // import Form from 'react-bootstrap/Form'
+// import Button from 'react-bootstrap/Form'
+// import Card from 'react-bootstrap/Card'
 
 
 
-    //onChange event is used to save the values from the input field.
-    constructor(props) {
-        super(props);
-        this.state = {
-            cityName: '',
-
-            // the returned data will be saved in the state.
-            cityData: {},
-            cityImage:'',
-        }
-    };
+// class InputFeild extends React.Component {
 
 
-    // this handler will take care of changing values.
-    cityNameHandeler = (e) => {
-        this.setState({
-            cityName: e.target.value
-        });
-    }
 
-    //form event : 
-    formSubmit = async (e) => {
-        e.preventDefault();
-        // ============= Consts ============== 
-        //     https://eu1.locationiq.com/v1/search.php?key=       q=${     }&format=json
-        const dataUrl = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_IQ_KEY}&q=${this.state.cityName}&format=json`;
-        // console.log(url);
-        const mapUrl= `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_IQ_KEY}&q=${this.state.cityData.lat,this.state.cityData.long}&zoom=1-18`;
+//     //onChange event is used to save the values from the input field.
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             cityName: '',
 
-        // ===== sending axios along with the url will send the key and the query parameter.
-        const response = await axios.get(dataUrl,mapUrl);
-        console.log(response.data[0]);// this line will get the first object that matches the word entered in the field.
-
-        this.setState({
-            cityData: response.data[0],
-            cityImage: response.data[0],
-        });
-    }
-
-    // the Form will hold the data , explore button will send the value in the request.
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.formSubmit}>
-                    <input type='text' onChange={this.cityNameHandeler} placeholder='Enter city name' />
-                    <input type='submit' value='Explore' />
-                </form>
+//             // the returned data will be saved in the state.
+//             cityData: {},
+//             cityImage:'',
+//         }
+//     };
 
 
-                <div>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src={this.state.cityImage} />
-                        <Card.Body>
-                            <Card.Title>City Information :</Card.Title>
-                            <Card.Text>
-                                {this.state.cityData.display_name}
-                            </Card.Text>
-                            <Card.Text>
-                                lat:{this.state.cityData.lat}
-                            </Card.Text>
-                            <Card.Text>
-                                long:{this.state.cityData.log}
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                    </Card>
+//     // this handler will take care of changing values.
+//     cityNameHandeler = (e) => {
+//         this.setState({
+//             cityName: e.target.value
+//         });
+//     }
 
-                </div>
+//     //form event : 
+//     formSubmit = async (e) => {
+//         e.preventDefault();
+//         // ============= Consts ============== 
+//         //     https://eu1.locationiq.com/v1/search.php?key=       q=${     }&format=json
+//         const dataUrl = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_IQ_KEY}&q=${this.state.cityName}&format=json`;
+//         // console.log(url);
+//         const mapUrl= `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_IQ_KEY}&q=${this.state.cityData.lat,this.state.cityData.long}&zoom=1-18`;
 
-                {/* <Form onSubmit={this.formSubmit}>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>City</Form.Label>
-                        <Form.Control type='text' placeholder='Enter city name'  onChange={this.cityNameHandeler} />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Explore
-                    </Button>
-                </Form> */}
-            </div>
-        );
-    }
+//         // ===== sending axios along with the url will send the key and the query parameter.
+//         const response = await axios.get(dataUrl,mapUrl);
+//         console.log(response.data[0]);// this line will get the first object that matches the word entered in the field.
 
-}
+//         this.setState({
+//             cityData: response.data[0],
+//             cityImage: response.data[0],
+//         });
+//     }
 
-export default InputFeild;
+//     // the Form will hold the data , explore button will send the value in the request.
+//     render() {
+//         return (
+//             <div>
+//                 <form onSubmit={this.formSubmit}>
+//                     <input type='text' onChange={this.cityNameHandeler} placeholder='Enter city name' />
+//                     <input type='submit' value='Explore' />
+//                 </form>
+
+
+//                 <div>
+//                     <Card style={{ width: '18rem' }}>
+//                         <Card.Img variant="top" src={this.state.cityImage} />
+//                         <Card.Body>
+//                             <Card.Title>City Information :</Card.Title>
+//                             <Card.Text>
+//                                 {this.state.cityData.display_name}
+//                             </Card.Text>
+//                             <Card.Text>
+//                                 lat:{this.state.cityData.lat}
+//                             </Card.Text>
+//                             <Card.Text>
+//                                 long:{this.state.cityData.log}
+//                             </Card.Text>
+//                             <Button variant="primary">Go somewhere</Button>
+//                         </Card.Body>
+//                     </Card>
+
+//                 </div>
+
+//                 {/* <Form onSubmit={this.formSubmit}>
+//                     <Form.Group className="mb-3" controlId="formBasicEmail">
+//                         <Form.Label>City</Form.Label>
+//                         <Form.Control type='text' placeholder='Enter city name'  onChange={this.cityNameHandeler} />
+//                     </Form.Group>
+//                     <Button variant="primary" type="submit">
+//                         Explore
+//                     </Button>
+//                 </Form> */}
+//             </div>
+//         );
+//     }
+
+// }
+
+// export default InputFeild;
