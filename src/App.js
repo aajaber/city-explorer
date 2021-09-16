41ac9817f41348251f44a3322a1d978e8dd715d0
aajaber <aajaber.97@gmail.com>
2021-09-16 22:04:51 +0300

    lab 6 update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
-import './App.css';
-import axios from 'axios';
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-
-
+import "./App.css";
+import axios from "axios";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 class App extends React.Component {
-
-
   constructor(props) {
     super(props);
     this.state = {
-
-      cityName: '',
+      cityName: "",
       cityData: {},
       displayed: false,
       errormessege: false,
       errorWarning: "",
       errorName: "",
       weatherList: [],
-    }
+    };
   }
-
 
   formSubmit = async (e) => {
     e.preventDefault();
@@ -40,22 +34,18 @@ class App extends React.Component {
       this.setState({
         cityData: dataResponse.data[0],
         weatherList: weatherDataResponse.data[0],
-        displayed: true
-
-      })
-    }
-    catch (error) {
+        displayed: true,
+      });
+    } catch (error) {
       if (this.state.cityName) {
         this.setState({
-          
           errormessege: true,
           errorName: error.message,
           errorWarning: "Enter a city name within your zone",
           data: "",
           cityName: "",
         });
-      }
-      else {
+      } else {
         this.setState({
           errormessege: true,
 
@@ -66,49 +56,48 @@ class App extends React.Component {
         });
       }
     }
-  }
-
+  };
 
   cityNameHandeler = (event) => {
     if (event.target.value) {
       this.setState({
-        cityName: event.target.value
-      })
-    }
-    else {
+        cityName: event.target.value,
+      });
+    } else {
       this.setState({
         errormessege: true,
         cityName: "",
       });
     }
-
-  }
+  };
 
   render() {
-    const mapUrl = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_IQ_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}`;
+    const mapUrl = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_IQ_KEY}&center=31.9515694,35.9239625`;
+
+    // const mapUrl = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_IQ_KEY}&center={$},{$}`;
 
     return (
       <div>
-        <h1 style={{ textAlign: 'center' }}>City Explorer</h1>
+        <h1 style={{ textAlign: "center" }}>City Explorer</h1>
 
         <Form onSubmit={this.formSubmit}>
-          <Form.Control type="text" placeholder="Enter City Name" onChange={this.cityNameHandeler} />
-          <Button variant="primary" type="submit">Submit</Button>
+          <Form.Control
+            type="text"
+            placeholder="Enter City Name"
+            onChange={this.cityNameHandeler}
+          />
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
         </Form>
 
-        <p style={{ textAlign: 'center' }}>{this.state.cityData.display_name}</p>
-        <p style={{ textAlign: 'center' }}>{this.state.cityData.lat}</p>
-        <p style={{ textAlign: 'center' }}>{this.state.cityData.lon}</p>
-        {
-          this.state.displayed && <img src={mapUrl}
-            alt='map'
-          />
-
-        }
-        {
-          this.state.errormessege &&
-          <p >Faild to load Data !</p>
-        }
+        <p style={{ textAlign: "center" }}>
+          {this.state.cityData.display_name}
+        </p>
+        <p style={{ textAlign: "center" }}>{this.state.cityData.lat}</p>
+        <p style={{ textAlign: "center" }}>{this.state.cityData.lon}</p>
+        {this.state.displayed && <img src={mapUrl} alt="map" />}
+        {this.state.errormessege && <p>Faild to load Data !</p>}
 
         <div>
           {this.state.weatherList.map((item) => {
@@ -125,6 +114,5 @@ class App extends React.Component {
       </div>
     );
   }
-
 }
 export default App;
